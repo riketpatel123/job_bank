44bcba29663c8cf53c41920f93bdb96d216c166a
riketpatel <riketpatel123@gmail.com>
2019-12-02 17:19:42 -0500

    fixup:  issues app.js

--- app.js
+++ app.js
@@ -19,13 +19,15 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 /**Define server running port */
 var PORT = process.env.PORT || 8000;
-app.use(helmet.frameguard({ action: "sameorigin" }));
+// app.use(helmet.frameguard({ action: "sameorigin" }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("X-Frame_Options","deny");
-  res.header("X-Frame_Options","sameorigin");
+  // res.header("X-Frame_Options","deny");
+  // res.header("X-Frame_Options","sameorigin");
+  // res.header("X-XSS-Protection","1; mode=block");
+  // res.header("Cache-Control","no-cache");
   next();
 });
 
